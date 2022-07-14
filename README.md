a1701fbb916eb66ffb8ba80f1e829294c3581cad
Alexander Solonovich <solonovich@gmail.com>
2022-07-14 19:40:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-exercises-JS-for-Dummies
+ Organizing my JavaScript knowledges by doing exercises from book "JavaScript for dummies"
---
+++ dummies.js
@@ -0,0 +1,27 @@
+// const firstNumber = prompt('Введите первое число','x');
+// const secondNumber = prompt('Введите второе число','y');
+// let total = firstNumber * secondNumber;
+// const sklejka = firstNumber + secondNumber;
+// const summa = Number(firstNumber) + Number(secondNumber);
+// alert('Склеили: ' + sklejka + '\nСложили: ' + summa + '\nПеремножили: ' + total);
+
+// var movie = 'Крестный отец';
+// function showGoodMovie () {
+//     alert (movie + ' - хороший фильм');
+// }
+// function showBadMovie () {
+//     movie = 'Джо Блэк';
+//     alert (movie + ' - плохой фильм');
+// }
+// showGoodMovie();
+// showBadMovie();
+// showGoodMovie();
+
+console.log(1+1);
+console.log(3 * 3);
+console.log(parseFloat('839'));
+console.log(parseInt('33.33333'));
+console.log(13+'12');
+console.log('13'+12);
+console.log("12" * 2);
+
